3f5eb19e0a1619982ebcab15b9d45b38efee36ba
Faeblaze <mjbstankovic27@gmail.com>
2017-11-23 13:02:45 +1100

    SCRIPTS

--- First game idea/Assets/Scripts/Bullet.cs
+++ First game idea/Assets/Scripts/Bullet.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float speed = 15f;
+    public Transform target;
+    public float damage = 1f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Vector3 dir = target.position - this.transform.localPosition;
+
+        float distThisFrame = speed * Time.deltaTime;
+
+        if (dir.magnitude <= distThisFrame)
+        {
+            DoBulletHit();
+        }
+        else
+        {
+            transform.Translate(dir.normalized * distThisFrame, Space.World);
+        }
+    }
+    void DoBulletHit()
+    {
+        target.getComponent<Enemy>().TakeDamage(damage);
+        Destroy(gameObject);
+    }
 }
